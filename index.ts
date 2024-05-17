f404d99d757c5c08d5a4d96d0556fc1c1fc97e38
Muhammad Qurban <qurbanbhutto11@gmail.com>
2024-05-17 13:22:29 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import inquirer from "inquirer"
+
+let answer:{
+    Sentence: string
+} = await inquirer.prompt(
+    [
+        {
+        name: "Sentence",
+        type: "string",
+        message: "Enter your sentence"
+        }
+    ]
+)
+let word = answer.Sentence.trim().split(" ")
+console.log(word)
+console.log(`Your sentence has  ${word.length} words`)
